c83e8edf4d9c6987809c5ecddcaa1406dd328932
Anaël <anael@barodine.fr>
2025-10-05 16:19:34 +0200

    feat: Transmission des infos de la requête à Python.

--- exec.php
+++ exec.php
@@ -1,10 +1,16 @@
 <?php
-print_r($_SERVER);
+$b64http = base64_encode(json_encode([
+    "method" => $_SERVER['REQUEST_METHOD'],
+    "target" => $_SERVER['REQUEST_URI'],
+    "protocol" => $_SERVER['SERVER_PROTOCOL'],
+    "headers" => getallheaders(),
+    "body" => file_get_contents('php://input')
+]));
 
 if (shell_exec("which python")) {
-    echo shell_exec("python server.py 2>&1");
+    echo shell_exec("python 2>&1 server.py $b64http");
 } elseif (shell_exec("which python3")) {
-    echo shell_exec("python3 server.py 2>&1");
+    echo shell_exec("python3 2>&1 server.py $b64http");
 } else {
     throw new Error("Python 3 is not installed on this server");
     // Apache routes to error 500 html page, this message shows only when the server is not Apache (PHP built-in server)
--- server.py
+++ server.py
@@ -1,1 +1,13 @@
-print("Le serveur virtuel Python est fonctionnel")
+import sys
+from base64 import b64decode
+from json import loads as bytes_to_json
+
+# Récupération des informations de la requête
+http = bytes_to_json(b64decode(sys.argv[1]))
+
+# ===============================
+# === Modifier à partir d'ici ===
+# ===============================
+
+# Affichage des informations de la requête.
+print(http)
