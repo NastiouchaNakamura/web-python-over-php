9d57bc21a289f0b754123a96fcdcc0ba6a30023a
Anaël <anael@barodine.fr>
2025-10-05 15:47:29 +0200

    feat: Affichage des erreurs Python dans la sortie standard.

--- exec.php
+++ exec.php
@@ -2,9 +2,9 @@
 print_r($_SERVER);
 
 if (shell_exec("which python")) {
-    echo shell_exec("python server.py");
+    echo shell_exec("python server.py 2>&1");
 } elseif (shell_exec("which python3")) {
-    echo shell_exec("python3 server.py");
+    echo shell_exec("python3 server.py 2>&1");
 } else {
     throw new Error("Python 3 is not installed on this server");
     // Apache routes to error 500 html page, this message shows only when the server is not Apache (PHP built-in server)
